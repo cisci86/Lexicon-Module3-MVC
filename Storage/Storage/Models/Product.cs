9ba8637cc2a8ae978efc9adb202ae40b5997e250
Cecilia Sjögren <cisci_13@hotmail.com>
2022-01-24 17:08:14 +0100

    Add project files.

---
+++ Storage/Storage/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+        [Required]
+        public string Catagory { get; set; }
+        public string Shelf { get; set; }
+        [Range(0,300)]
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Storage/Storage/Models/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.Models.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+
+    }
+}
