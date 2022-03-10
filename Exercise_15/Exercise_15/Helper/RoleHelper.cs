b854f6592b3b097c1d9a56e173d10362a43174e0
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-10 10:32:48 +0100

    Added static class for Stings

---
+++ Exercise_15/Exercise_15/Helper/RoleHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_15.NewFolder
+{
+    public static class RoleHelper
+    {
+        public static string GetAdminRole()
+        {
+            return "Admin";
+        }
+    }
+}
